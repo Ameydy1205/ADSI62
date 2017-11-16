2edc333d89673db4a588d8a8797da37d64de7363
Keeeeeeevin <kralexgc04@gmail.com>
2017-11-16 17:12:52 -0500

    intento de cambio 3php keeee

---
+++ 3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hola Compañeros</title>
+</head>
+<body>
+<h1>Esto es un titulo</h1>
+
+<p>Esto es lo que traigo de mi costa para que todo el mundo lo baile y el que se quede sentado sera multao y vamonos con el baile costeño pues… (hablando) 
+
+ayyyyyyy Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala sii 
+
+Arremangala arrempujala si Arremangala arrempujala no Arremangala arrempujala si Arremangala arrempujala no Arremangala arrempujala si Arremangala arrempujala no Arremangala arrempujala si Arremangala arrempujala no Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempu jala sii 
+
+Arremangala arrempujala si Arremangala arrempujala no Arremangala arrempujala si Arremangala arrempujala no Arremangala arrempujala si Arremangala arrempujala no Arremangala arrempujala si Arremangala arrempujala no 
+
+Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala Arremangala arrempujala sii y arremangala y arrempujala si señor (hablando) 
+
+(se repite todo 2 veces mas) 
+
+Otra vez el cachaz ixtleco esto es para todos mis compas de la costa y a mi amigos Ixtlecos ... adios by kevin</p>
+
+</body>
+</html>
